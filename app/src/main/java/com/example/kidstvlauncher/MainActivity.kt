e8a725ff07c325a16331ed6e10a071e9a2c8ffde
DevDewangan16 <dewangandev977@gmail.com>
2025-03-29 23:43:34 +0530

    improved app stability.

--- app/src/main/java/com/example/kidstvlauncher/MainActivity.kt
+++ app/src/main/java/com/example/kidstvlauncher/MainActivity.kt
@@ -6,13 +6,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.compose.rememberNavController
 import com.example.kidstvlauncher.ui.AppNavigation
 import com.example.kidstvlauncher.ui.theme.KidsTVLauncherTheme
 
